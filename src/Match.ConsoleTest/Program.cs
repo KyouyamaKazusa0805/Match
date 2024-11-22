9234a8461cbfbd28b23dd19a82780dee28878096
Kyouyama Kazusa <Kazusa0805@outlook.com>
2024-11-22 16:58:29 +0800

    Remove project.

--- src/Match.ConsoleTest/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using Match.Generating;
-
-var grid1 = Generator.Generate(6, 6, 12);
-var grid2 = Generator.Generate(6, 6, 12);
-var grid3 = Generator.Generate(6, 6, 12);
-Console.WriteLine(grid1!.ToString());
-Console.WriteLine(grid2!.ToString());
-Console.WriteLine(grid3!.ToString());
